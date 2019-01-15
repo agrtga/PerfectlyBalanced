688bda0cdc0578028e50abed90240d984faca25e
Anthony Trudeau <agrt.home@hotmail.com>
2019-01-14 20:30:29 -0500

    Add project files.

---
+++ PerfectlyBalanced/BalancedInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+
+namespace PerfectlyBalanced
+{
+    public sealed class BalancedInput
+    {
+        public BalancedInput(string input) 
+            => Input = input;
+
+        public string Input { get; }
+
+        public bool IsBalanced()
+        {
+            if (Input.Length > 0) {
+                var charGroups = Input.ToCharArray()
+                    .GroupBy(c => c)
+                    .ToDictionary(g => g.Key, g => g.Count());
+
+                int checkCount = charGroups.First().Value;
+                return charGroups.All(c => c.Value == checkCount);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ PerfectlyBalanced/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PerfectlyBalanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputs = new[] {
+                new BalancedInput("xxxyyyzzz"),
+                new BalancedInput("abccbaabccba"),
+                new BalancedInput("xxxyyyzzzz"),
+                new BalancedInput("abcdefghijklmnopqrstuvwxyz"),
+                new BalancedInput("pqq"),
+                new BalancedInput("fdedfdeffeddefeeeefddf"),
+                new BalancedInput("www"),
+                new BalancedInput("x"),
+                new BalancedInput("")
+            };
+
+            Array.ForEach(inputs, i => Console.WriteLine($"input({i.Input} => {i.IsBalanced().ToString()}"));
+        }
+    }
+}
